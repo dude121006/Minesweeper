3f8d9b946175c86fa260f1e498189050be0d0114
dude121006 <95970549+dude121006@users.noreply.github.com>
2024-12-03 21:55:04 +0530

    rendered a triangle

--- src/main.c
+++ src/main.c
@@ -9,7 +9,20 @@ static unsigned int CompileShader(unsigned int type, char* source)
     glShaderSource(id, 1, &source, NULL);
     glCompileShader(id);
     
-    //TODO: error handling
+    // querying the success of compilation
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+
+    if (result == GL_FALSE)
+    {
+        printf("The compilation for type %s was unsuccessful!\n", 
+                    (type == GL_VERTEX_SHADER) ? "vertex shader" : "fragment shader");
+
+        // delete the shader bcos compilation was unsuccessful
+        glDeleteShader(id);
+        return 0;
+    }
 
     return id;
 }
@@ -18,8 +31,8 @@ static unsigned int CreateShader(char* vertShaderSource, char* fragShaderSource)
 {
     unsigned int program = glCreateProgram();
     
-    unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
-    unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertShaderSource);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragShaderSource);
 
     glAttachShader(program, vs);
     glAttachShader(program, fs);
@@ -27,8 +40,17 @@ static unsigned int CreateShader(char* vertShaderSource, char* fragShaderSource)
     glLinkProgram(program);
     glValidateProgram(program);
     
+    int validateResult;
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &validateResult);
+    if (validateResult == GL_FALSE)
+    {
+        printf("Error in validation!\n");
+    }
+    
     glDeleteShader(vs);
     glDeleteShader(fs);
+    
+    return program;
 }
 
 
@@ -46,13 +68,15 @@ int main() {
         return -1;
     }
 
- 
     glfwMakeContextCurrent(window);
  
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         printf("Failed to initialize GLAD\n");
         return -1;
     }
+
+
+
 
     
     float vertPos[6] = {
@@ -64,6 +88,10 @@ int main() {
     // creates 1 buffer and gives it a tag of "buffer"
     unsigned int buffer;
     glGenBuffers(1, &buffer);
+    
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
     // binds the buffer and gives it a type of array buffer
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -83,6 +111,37 @@ int main() {
     // enable vertex attributes
     glEnableVertexAttribArray(0);
 
+
+    // layout(location = 0) specifies the index is at 0 in attribPointer
+    char vertexShader[] = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+    
+
+    char fragmentShader[] = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(0.0, 0.0, 1.0f, 1.0f);\n"
+        "}\n";
+
+
+    unsigned int shaderProgram = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shaderProgram);
+
+    // GLenum err;
+    // while ((err = glGetError()) != GL_NO_ERROR) {
+    //     printf("OpenGL error: %d\n", err);
+    // }
 
  
     while (!glfwWindowShouldClose(window)) {
